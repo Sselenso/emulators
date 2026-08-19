cc050f96822533c948c06f4263118f4e906459f0
Sselenso <122260649+Sselenso@users.noreply.github.com>
2026-08-19 15:53:14 +0300

    обновил

--- router-admin/js/utils.js
+++
@@ -1,5 +0,0 @@
-// utils.js
-// Можно добавить сюда функции валидации, форматирования MAC-адресов и т.д.
-function formatMac(mac) {
-  return mac.replace(/[^0-9A-Fa-f]/g, '').match(/.{1,2}/g)?.join(':') || mac;
-}
